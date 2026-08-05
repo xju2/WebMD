a412356695464567259bb6a93cd519efa393988a
Xiangyang Ju <xiangyang.ju@gmail.com>
2026-08-05 10:19:37 -0700

    Use icon for code copy button

--- src/App.svelte
+++ src/App.svelte
@@ -2179,8 +2179,13 @@
             aria-label="Copy code block"
             title="Copy code"
             type="button"
-            on:click={() => copyCodeBlock(block.text)}>Copy</button
+            on:click={() => copyCodeBlock(block.text)}
           >
+            <svg aria-hidden="true" viewBox="0 0 24 24">
+              <rect x="8" y="7" width="10" height="13" rx="2" />
+              <path d="M6 17H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v1" />
+            </svg>
+          </button>
         </span>
         <pre><code>{block.text}</code></pre>
       </div>
